de17186cd6235b1359c031701a78e5d52f4e3132
sumantopal07 <sumantopal07@gmail.com>
2021-04-07 18:32:05 +0530

    export import statements

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const Books = [
+	{
+		id: 1,
+		author: 'Amelia Hepworth',
+		title: 'I Love You to the Moon and Back',
+		img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg'
+	},
+	{
+		id: 2,
+		author: 'Shanon Olsen',
+		title: 'Our Class is a Family',
+		img: 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg'
+	}
+]
