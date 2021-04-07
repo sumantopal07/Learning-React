168e25bf0f0b03b587f148c31ca53801262d8279
sumantopal07 <sumantopal07@gmail.com>
2021-04-07 21:46:23 +0530

    dev environment  setup

--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
-/* eslint-disable no-unused-vars */
-
 import React from 'react';
-import ReactDom from 'react-dom';
-import './index.css'
-import { Books } from './books';
-import SpecificBook from './Book';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-function BookList() {
-	const printAuthor = (author) => {
-		console.log(author);
-	};
-	const testFunction = (e) => {
-		console.log(e.target);
-	};
-	return <section className='booklist'>
-		{
-			Books.map((book) => {
-				return (
-					<SpecificBook  {...book} key={book.id} >
-						<p>Book  Number : {book.id}</p>
-						<p>{Math.floor(Math.random(book.id) * 100)} Number of books sold</p>
-						<button type='button' onClick={() => printAuthor(book.author)} >test1</button>
-						<button type='button' onClick={testFunction} >test2</button>
-					</SpecificBook>
-				)
-			}
-			)}
-	</section>
-}
-ReactDom.render(<BookList />, document.getElementById('root'));
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
