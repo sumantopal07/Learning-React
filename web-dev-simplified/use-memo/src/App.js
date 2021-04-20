e8d809adabccb179b1b8ccab3e3ec137fa1fe55f
sumantopal07 <sumantopal07@gmail.com>
2021-04-20 21:46:33 +0530

    caption added

--- web-dev-simplified/use-memo/src/App.js
+++ web-dev-simplified/use-memo/src/App.js
@@ -1,4 +1,47 @@
-import React, { useState, useMemo,useEffect } from 'react';
+// the two big use
+
+// cases for a use memo the first and most
+
+// obvious is when you want to make a slow
+
+// function we wrap it in this use memo so
+
+// that doesn't recompute every single time
+
+// you render your component and it only
+
+// computes when you actually need the
+
+// value from that function since the
+
+// inputs actually changed the second use
+
+// case which is a little bit trickier is
+
+// this idea of a referential equality
+
+// whenever you want to make sure the
+
+// reference of an object or an array is
+
+// exactly the same as it was the last time
+
+// you rendered if none of the internal
+
+// workings changed you're gonna want to
+
+// use memo here to make sure that you only
+
+// update the reference of that object
+
+// whenever the actual contents of the
+
+// object change instead of updating every
+
+// single time you render and that's all
+
+// there is to the used memo hook
+import React, { useState, useMemo, useEffect } from 'react';
 function slowFunction(num) {
 	console.log("Calling Slow Function");
 	for (let i = 0; i <= 1e9; i++) {
@@ -62,9 +105,9 @@ function App() {
 	// 		backgroundColor: dark ? 'black' : 'white',
 	// 		color: dark ? 'white' : 'black'
 	// 	};
-	useEffect(()=>{
+	useEffect(() => {
 		console.log('theme changes');
-	},[themeStyles]);
+	}, [themeStyles]);
 	return (
 		<>
 			<input type="number"
@@ -79,5 +122,6 @@ function App() {
 		</>
 	);
 }
+
 
 export default App;
